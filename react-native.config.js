d52527779406ded568e6fc5c88f9aba6a5cc0166
Toyin46 <joshuatoyin55@gmail.com>
2026-06-28 07:21:44 -0700

    Fix ffmpeg package variant to audio

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dependencies: {
+      'ffmpeg-kit-react-native': {
+        platforms: {
+          android: {
+            packageName: 'audio',
+          },
+        },
+      },
+    },
+  };
