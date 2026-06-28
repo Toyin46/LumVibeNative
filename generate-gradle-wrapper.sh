7ff6bbb0cfa9413353654d1431f0e64d08a90127
Toyin46 <joshuatoyin55@gmail.com>
2026-06-27 23:38:15 -0700

    Add prebuild script to download gradle jar on EAS server

---
+++ generate-gradle-wrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd android
+curl -o gradle/wrapper/gradle-wrapper.jar https://github.com/gradle/gradle/raw/v8.3.0/gradle/wrapper/gradle-wrapper.jar
+echo "Gradle wrapper jar downloaded successfully"
